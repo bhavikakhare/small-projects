89be5db9130f91c6ae9793e6e764eb372391dd99
Bhavika Khare <32902603+bhavikakhare@users.noreply.github.com>
2021-11-15 14:24:39 -0600

    Create indexer.pl

---
+++ perl/indexer.pl
@@ -0,0 +1,19 @@
+# document header
+
+# read RI file into matrix or sth
+
+# ask for query
+
+# proprocess query
+
+# calculate cosim with all documents each word is in & store in list -> order it
+
+# map file-number to the page-link
+
+# print the relevant links
+
+#####
+
+# make an HTML page
+
+# make a form on it &  server
